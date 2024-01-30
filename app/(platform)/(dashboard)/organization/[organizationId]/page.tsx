07eeb0ec155c8c49943b9f1eed9d02344d2f6ed3
YashGupt29 <yashnitagartala8@gmail.com>
2024-01-30 17:37:02 +0530

    done popover

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,16 +1,15 @@
 import { db } from "@/lib/db";
-import { Board } from "./board";
-import { Form } from "./form";
-
+import { Info } from "./_components/info";
+import { useOrganization } from "@clerk/nextjs";
+import { Separator } from "@/components/ui/separator";
+import BoardList from "./_components/board-list";
 const OrganizationIdPage = async () => {
-  const boards = await db.boarding.findMany();
   return (
-    <div className="flex flex-col space-y-4">
-      <Form />
-      <div className="space-y-2">
-        {boards.map((boarding) => (
-          <Board key={boarding.id} title={boarding.title} id={boarding.id} />
-        ))}
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
       </div>
     </div>
   );
--- lib/create-safe-action.ts
+++ lib/create-safe-action.ts
@@ -1,6 +1,4 @@
 import { z } from "zod";
-import { CreateBoard } from "../actions/create-board/schema";
-
 export type FieldErrors<T> = {
   [K in keyof T]?: string[];
 };
